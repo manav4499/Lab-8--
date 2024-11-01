4c6e64ad5af6f82ad227a83b2bce74607b80b7f5
KhanjanPadaliya <padaliyakhanjan@gmail.com>
2024-11-01 13:33:20 -0400

    Final Changes

--- lab-08/src/App.jsx
+++ lab-08/src/App.jsx
@@ -9,7 +9,10 @@ function App() {
   return (
     <>
       <h1>Team Member Profile</h1>
+      <div class="ComponantList">
       <MemberComponent name="Manav Chaudhary" dob="11/10/2004" favFood="Vada Pav" favEmoji="smily" birthPlace="Mehsana"/>
+      <MemberComponent name="Khanjan Padaliya" dob="11/01/2005" favFood="Dabeli" favEmoji="Kiss" birthPlace="Talala Gir"/>
+      </div>
     </>
   )
 }
